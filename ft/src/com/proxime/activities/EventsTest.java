ab97d6dc9f08af8226b7c049adcb1fd0eff811e7
Jijesh Mohan <jijeshmohan@gmail.com>
2011-01-06 15:07:00 +0530

    Added initial functional test

--- ft/src/com/proxime/activities/EventsTest.java
+++ ft/src/com/proxime/activities/EventsTest.java
@@ -1,6 +1,8 @@
 package com.proxime.activities;
 
 import android.test.ActivityInstrumentationTestCase2;
+import com.jayway.android.robotium.solo.Solo;
+import com.proxime.entities.Event;
 
 /**
  * This is a simple framework for a test of an Application.  See
@@ -12,10 +14,53 @@ import android.test.ActivityInstrumentationTestCase2;
  * -e class com.proxime.activities.EventsTest \
  * com.proxime.tests/android.test.InstrumentationTestRunner
  */
-public class EventsTest extends ActivityInstrumentationTestCase2<Events> {
+public class EventsTest extends ActivityInstrumentationTestCase2 {
 
-    public EventsTest() {
-        super("com.proxime.activities", Events.class);
+    private static final String TARGET_PACKAGE_ID = "com.proxime.activities";
+    private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.proxime.activities.Events";
+
+
+    private static Class<?> launcherActivityClass;
+
+    static {
+            try {
+                launcherActivityClass = Class.forName(LAUNCHER_ACTIVITY_FULL_CLASSNAME);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    public EventsTest() throws ClassNotFoundException{
+         super(TARGET_PACKAGE_ID, launcherActivityClass);
     }
 
+    private Solo solo;
+
+    protected void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+
+    }
+
+    private String getNewEventName() {
+        return "Sample " + System.currentTimeMillis();
+    }
+
+    public void testCanCreateNewEvents(){
+
+        int previous_count = solo.getCurrentListViews().get(0).getCount();
+        String eventname=getNewEventName();
+
+        solo.clickOnButton(0);
+        solo.waitForActivity("EditEvent",1000);
+        assert(solo.searchText("New Event"));
+
+        solo.enterText(0, eventname);
+        solo.clickOnButton("Save");
+        solo.waitForActivity("Events",1000);
+
+        assertEquals(previous_count+1,solo.getCurrentListViews().get(0).getCount());
+        Event itemAtPosition = (Event)solo.getCurrentListViews().get(0).getItemAtPosition(previous_count);
+        assertEquals(eventname,itemAtPosition.getName());
+
+    }
 }
