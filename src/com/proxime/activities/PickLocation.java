9cabf41bbfd2044c83c91423836c3d26339db713
vineeth-venu <vineeth.venu@gmail.com>
2011-01-17 02:22:02 +0530

    UI - Map now has a marker for user-friendliness <vineeth>

--- src/com/proxime/activities/PickLocation.java
+++ src/com/proxime/activities/PickLocation.java
@@ -1,6 +1,10 @@
 package com.proxime.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,20 +21,35 @@ import java.util.List;
 
 public class PickLocation extends MapActivity
 {
+    private GeoPoint currentlySelectedLocationGeoPoint;
+
     public class MapOverlay extends com.google.android.maps.Overlay
     {
+
+
+        @Override
+        public void draw(Canvas canvas, MapView mapView, boolean b)
+        {
+            super.draw(canvas, mapView, b);
+            Point screenPts = new Point();
+            mapView.getProjection().toPixels(currentlySelectedLocationGeoPoint, screenPts);
+            Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.proxime);
+            canvas.drawBitmap(bmp, screenPts.x, screenPts.y-48, null);
+        }
+
         @Override
         public boolean onTap(GeoPoint geoPoint, MapView mapView)
         {
             String formattedAddress ="";
                 try
                 {
+                    currentlySelectedLocationGeoPoint = geoPoint;
                     formattedAddress = new GeoCoderServiceUtility().getFormattedAddress(geoPoint);
                     ((TextView)(findViewById(R.id.refinedSearch))).setText(formattedAddress);
                     Intent intent =new Intent();
                     intent.putExtra("formattedAddress",formattedAddress);
                     intent.putExtra("latitude",geoPoint.getLatitudeE6()/1E6);
-                    intent.putExtra("longitude",geoPoint.getLongitudeE6()/1E6);
+                    intent.putExtra("longitude", geoPoint.getLongitudeE6() / 1E6);
                     setResult(EditLocation.MAP_REQUEST_CODE, intent);
 
                 } catch (JSONException e)
@@ -60,8 +79,9 @@ public class PickLocation extends MapActivity
         String formattedAddress = (getIntent().getStringExtra("formattedAddress"));
         ((TextView)(findViewById(R.id.refinedSearch))).setText(formattedAddress);
         GeoPoint intiailLocation = new GeoPoint(((int)( latitude*1E6)),((int)( longitude*1E6)));
-        mapView.getController().setCenter(intiailLocation);
+        mapView.getController().animateTo(intiailLocation);
         mapView.getController().setZoom(18);
+        currentlySelectedLocationGeoPoint  = intiailLocation;
     }
 
     private void setOverlays(MapView mapView)
@@ -83,7 +103,8 @@ public class PickLocation extends MapActivity
 
     private void hookUpListeners()
     {
-         ((Button)(findViewById(R.id.setLocationFromMap))).setOnClickListener(new View.OnClickListener(){
+         ((Button)(findViewById(R.id.setLocationFromMap))).setOnClickListener(new View.OnClickListener()
+         {
 
              public void onClick(View view)
              {
